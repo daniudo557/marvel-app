79f59db847788eeeb3cf35ce41dcbe2a4b518d71
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-04 19:48:26 -0300

    Improves navbar creating its own component

---
+++ src/components/navbar/burguerMenu.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import './styles.scss'
+
+const BurguerMenu = ({ items }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  const renderBurguerItems = () => (
+    items.map((item, index) => (
+      <Link
+        className='marvelDefaultText menuItem'
+        style={{ fontSize: 22, padding: '8px 0px 8px 0px' }}
+        key={index}
+        to={item.path}
+        onClick={() => setSidebarOpen(false)}
+      >
+        {item.text}
+      </Link>
+    ))
+  )
+
+  const renderOpenedBurguer = () => (
+    <div className='sideBarMenu'>
+      {renderBurguerButton()}
+      {renderBurguerItems()}
+    </div>
+  )
+
+  const renderBurguerButton = () => (
+    <div className='burgerButtonContainer'>
+      <div
+        className='burgerButton'
+        onClick={() => setSidebarOpen(!sidebarOpen)}
+      >
+        {sidebarOpen ? 'X' : '|||'}
+      </div>
+    </div>
+  )
+  return sidebarOpen ? renderOpenedBurguer() : renderBurguerButton()
+}
+export default BurguerMenu
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -4,26 +4,53 @@ import { Link } from 'react-router-dom'
 
 import paths from '../../routes/paths'
 import { useWindowDimensions } from '../../functions/utils'
-import './styles.css'
+import BurguerMenu from './burguerMenu'
+import './styles.scss'
 
 const Navbar = () => {
   const { width } = useWindowDimensions()
   const isMobile = width <= breakpoints.md
+  const menuItems = [
+    { text: 'Quadrinhos', path: paths.home },
+    { text: 'Personagens', path: paths.comics },
+    { text: 'Minha lista', path: paths.home }
+  ]
+
   const renderMobileNavBar = () => (
-    <div className='navBar' style={{ height: 32 }}>
-      <div className='marvelLogoMobile'>
-        <Link to={paths.home} style={{ textDecoration: 'none' }}>
-          <p className='logoText'>M</p>
+    <div className='navBar navBar-mobile'>
+      <div className='marvelLogo marvelLogo-mobile'>
+        <Link
+          className='marvelDefaultText'
+          style={{ paddingRight: 8, paddingLeft: 8 }}
+          to={paths.home}
+        >
+          M
         </Link>
       </div>
+      <BurguerMenu items={menuItems} />
     </div>
   )
 
   const renderDesktopNavBar = () => (
     <div className='navBar'>
-      <Link to={paths.home} style={{ textDecoration: 'none' }}>
+      <Link
+        style={{ height: '100%' }}
+        to={paths.home}
+      >
         <img src={images.logo} className='marvelLogo' alt='logo' />
       </Link>
+      <div className='menuLine'>
+        {menuItems.map((item, index) => (
+          <Link
+            to={item.path}
+            className='marvelDefaultText menuItem'
+            key={index}
+          >
+            {item.text}
+          </Link>
+        ))}
+
+      </div>
     </div>
   )
 
