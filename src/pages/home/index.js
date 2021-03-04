53575af78b1f74ea971206d7c4a50bb89158c059
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-03 23:19:12 -0300

    Includes ternary to apply different flexDirection according to device width

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { images } from '../../themes'
-import './styles.css'
-import paths from '../../routes/paths'
+import { images, breakpoints } from '../../themes'
 import { Link } from 'react-router-dom'
 
+import paths from '../../routes/paths'
+import { useWindowDimensions } from '../../functions/utils'
+import './styles.css'
+
 const Home = () => {
+  const { width } = useWindowDimensions()
+  const isMobile = width <= breakpoints.md
   return (
     <>
       <header className='App-header'>
-        <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div style={{ display: 'flex', flexDirection: isMobile ? 'column' : 'row' }}>
           <img src={images.header} className='slide-right App-logo' alt='logo' />
           <p className='appear' style={{ alignSelf: 'center', maxWidth: 500 }}>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
