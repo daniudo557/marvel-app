2d8054845b2daa4da94fc0dc06c7de135ddd898f
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-04 15:29:28 -0300

    Includes render of navbar in routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import paths from './paths'
 import Home from '../pages/home'
 import Comics from '../pages/comics'
+import Navbar from '../components/navbar'
 
 export default function Routes () {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Navbar />
       <Route path={paths.home} exact component={Home} />
       <Route path={paths.comics} component={Comics} />
     </BrowserRouter>
