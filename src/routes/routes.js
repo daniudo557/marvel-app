382ea45b2101b4c97cb99a0d7b30202b8e5b293c
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-05 19:27:43 -0300

    Removes global styles from routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,16 +5,13 @@ import paths from './paths'
 import Home from '../pages/home'
 import Comics from '../pages/comics'
 import Navbar from '../components/navbar'
-import GlobalStyles from '../components/globalStyles'
 
 export default function Routes () {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navbar />
-      <GlobalStyles>
-        <Route path={paths.home} exact component={Home} />
-        <Route path={paths.comics} component={Comics} />
-      </GlobalStyles>
+      <Route path={paths.home} exact component={Home} />
+      <Route path={paths.comics} component={Comics} />
     </BrowserRouter>
   )
 }
