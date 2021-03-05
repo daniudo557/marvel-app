71b760f98b3957e8fd55c7ab2b4473c8f87ab894
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-04 21:46:42 -0300

    Improves css classes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { images, breakpoints } from '../../themes'
-import { Link } from 'react-router-dom'
 
+import { images, breakpoints } from '../../themes'
 import { useWindowDimensions } from '../../functions/utils'
-import paths from '../../routes/paths'
-import './styles.css'
+import './styles.scss'
 
 const Home = () => {
   const { width } = useWindowDimensions()
-  const isMobile = width <= breakpoints.md
+
+  const isDesktop = width > breakpoints.md
+  const isTablet = width > breakpoints.sm && width <= breakpoints.md
+  const isMobile = width <= breakpoints.sm
+
+  const animatedImageDesktopClass = 'slide-right animatedImage'
+  const animatedImageTabletClass = 'slide-right animatedImage-tablet'
+  const animatedImageMobileClass = 'slide-right animatedImage-mobile'
+
+  const getAnimatedImageClass = () => {
+    if (isDesktop) return animatedImageDesktopClass
+    else if (isTablet) return animatedImageTabletClass
+    else if (isMobile) return animatedImageMobileClass
+  }
 
   return (
     <>
@@ -18,15 +29,24 @@ const Home = () => {
           style={{ backgroundImage: `url(${images.headerBackground})` }}
         />
       </div>
-      <header className='App-header'>
-        <div className={isMobile ? 'welcome-section-container-mobile' : 'welcome-section-container'}>
-          <img src={images.header} className='slide-right animated-image' alt='logo' />
+      <section className='welcomeSection'>
+        <div
+          className={isDesktop
+            ? 'welcomeSectionContainer'
+            : 'welcomeSectionContainer-mobile'}
+        >
+          <img
+            src={images.header}
+            className={getAnimatedImageClass()}
+            alt='logo'
+          />
           <p className='appear' style={{ alignSelf: 'center', maxWidth: 500 }}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum is simply dummy text of
+            the printing and typesetting industry.
           </p>
         </div>
-      </header>
-      <section style={{ backgroundColor: 'red', height: 500 }}>
+      </section>
+      <section style={{ backgroundColor: 'red', height: '90vh', width: '100%' }}>
         Secion
       </section>
     </>
