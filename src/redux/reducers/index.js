86573e1bc68e123724f89d5c45fbd4d3996bf489
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-07 11:30:50 -0300

    Removes old reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux'
-import { reducers as counterReducers } from './counter'
 import { reducers as comicsReducers } from './comics'
 
 const reducers = combineReducers({
-  counterReducers,
   comicsReducers
 })
 
--- src/redux/selectors/counter.js
+++
@@ -1,5 +0,0 @@
-const selectors = {
-  getCounter: state => state.counterReducers.counter
-}
-
-export { selectors }
