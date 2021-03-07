dcc43c44918c7dfc91f9d664662dbe6b8bec44a5
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-07 00:15:33 -0300

    Includes redux in list page

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 
+import { useSelector } from 'react-redux'
+import { selectors } from '../../redux/selectors/comics'
+
 import Banner from '../../components/banner'
+import Section from '../../components/section'
 import { images } from '../../themes'
-const List = (props) => {
-  const { params } = props.match
-  console.log(params)
+
+const List = () => {
+  const comics = useSelector(selectors.getComics)
+
   return (
     <div className=''>
-      <Banner image={images.list} />
+      <Banner
+        image={images.list}
+        text='Consulte sua lista de quadrinhos'
+      />
+      <Section
+        customStyles={{ backgroundColor: '#F5F5F5' }}
+        title='Consulte sua lista de quadrinhos'
+      >
+        {comics.map((comic, index) => (
+          <div key={index} style={{ padding: 32 }}>
+            <h1>Comic title: {comic.comicDetails.title}</h1>
+            <h1>Number of comics: {comic.numberOfComics}</h1>
+          </div>
+        ))}
+      </Section>
     </div>
   )
 }
