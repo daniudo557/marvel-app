040555614d5e465c017f32dd4215cf9bb49fbe34
Daniel Macedo <daniel.macedo05@hotmail.com>
2021-03-06 23:57:29 -0300

    Improves design of search bar

--- src/components/searchBar/index.js
+++ src/components/searchBar/index.js
@@ -1,12 +1,24 @@
 import React from 'react'
+
 import './styles.scss'
-import Button from '../../components/button'
+
+import { getBreakpoints, useWindowDimensions } from '../../functions/utils'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const SearchBar = ({ placeholder, onChange }) => {
+  const { width } = useWindowDimensions()
+  const { isTablet, isMobile } = getBreakpoints(width)
+
   return (
-    <form className='searchBarContainer'>
-      <input type='text' onChange={onChange} placeholder={placeholder} />
-      <Button />
+    <form className={`searchBar ${(isTablet || isMobile) && 'searchBar-mobile'}`}>
+      <FontAwesomeIcon
+        style={{ alignSelf: 'center', fontSize: 26 }}
+        color='#F0141E'
+        icon={faSearch}
+      />
+      <input type='text' style={{ width: '100%' }} onChange={onChange} placeholder={placeholder} />
     </form>
   )
 }
